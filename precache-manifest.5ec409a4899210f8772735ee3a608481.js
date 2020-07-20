e6e37ad91b593743c35f1e76483e17cfe4cd1bbe
aoseledec <artemij.oseledets@yandex.ru>
2020-07-20 15:54:50 +0800

    Updates

---
+++ precache-manifest.5ec409a4899210f8772735ee3a608481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c173f0a5559d87dfe9a65e4d1ac56ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a479ffae147548905183",
+    "url": "/static/css/main.310d1f68.chunk.css"
+  },
+  {
+    "revision": "479e076b72a88e953359",
+    "url": "/static/js/2.797a5009.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.797a5009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a479ffae147548905183",
+    "url": "/static/js/main.da42d868.chunk.js"
+  },
+  {
+    "revision": "4ef0cbba6b21b3aec743",
+    "url": "/static/js/runtime-main.930365f7.js"
+  }
+]);
